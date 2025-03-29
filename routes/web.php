8b2dfc5be0e3b94220385caec7b314e118371886
Ejine M. <ejinemangcobihon0727@gmail.com>
2025-03-29 15:16:24 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users', 'UserController@index'); // List users
+    $router->post('/users', 'UserController@store'); // Add user
+    $router->get('/users/{id}', 'UserController@show'); // Get user by ID
+    $router->put('/users/{id}', 'UserController@update'); // Update user
+    $router->patch('/users/{id}', 'UserController@update'); // Partial update
+    $router->delete('/users/{id}', 'UserController@destroy'); // Delete user
+});
